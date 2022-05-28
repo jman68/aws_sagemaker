e6e34aed1ba3cd5721f9e176aeb005c662510391
jman68 <106361181+jman68@users.noreply.github.com>
2022-05-28 19:32:52 -0400

    Update predict.R

--- predict.R
+++ predict.R
@@ -2,15 +2,14 @@ library(ranger)
 library(jsonlite)
 
 # download model file from S3 into /tmp folder
-print("Is this even happening?")
 system("aws s3 cp s3://r-testing/model/model.Rds /tmp/model.Rds")
 
 handler <- function(body, ...) {
   # load model
-  print("Trying to copy model object into tmp folder")
-  system("aws s3 cp s3://r-testing/model/model.Rds /tmp/model.Rds")
-  print(paste("The model objects exists:", file.exists("/tmp/model.Rds")))
   model <- readRDS("/tmp/model.Rds")
+  # parse JSON inputs
+  print(str(body))
+  body = as.data.frame(body)
   # predict with loaded model
   predictions <- predict(model, data = body)
   # return response with predictions payload
