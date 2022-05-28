e5e00f58e9570a4921ec420ce72014553a6c9fc8
jman68 <106361181+jman68@users.noreply.github.com>
2022-05-28 14:15:11 -0400

    Add files via upload

---
+++ predict.R
@@ -0,0 +1,21 @@
+library(ranger)
+library(jsonlite)
+
+# download model file from S3 into /tmp folder
+system("aws s3 cp ${S3_MODEL_URI} /model/abalone_fit.rds")
+
+handler <- function(body, ...) {
+  data <- fromJSON(txt = body)
+  # load model
+  model <- readRDS("/tmp/model.rds")
+  # predict with loaded model
+  predictions <- predict(model, data = data)
+  # return response with predictions payload
+  return(
+    list(
+      statusCode = 200,
+      headers = list("Content-Type" = "application/json"),
+      body = toJSON(list(predictions = predictions))
+    )
+  )
+}
